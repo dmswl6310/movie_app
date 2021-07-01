4c509a234671df22949defcca2f0a42b14ec033c
Eunji <ejhwang@seegene.com>
2021-07-01 20:43:22 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import Movie from './Movie';
 
 class App extends React.Component{
   state={
@@ -11,16 +12,20 @@ class App extends React.Component{
       data:{
         data:{movies}
       }
-    } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json")
-    this.setState({movies, isLoading:false});
+    } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating")
+    this.setState({movies,isLoading:false});
   };
   componentDidMount(){
     this.getMovies();
   }
   render(){
-    const {isLoading} = this.state;
-    return <div>{isLoading?"Loading...":"We are ready"}</div>;
-  }
+    const {isLoading,movies} = this.state;
+    return (
+      <section class="container">
+      {isLoading?"Loading...":movies.map(movie=>{
+      return <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />
+    })}</section>
+    )
 }
-
+}
 export default App;
